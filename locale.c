eed578c671cea6b2700469233e6a62da0281dfc7
Karl Williamson <khw@cpan.org>
2023-04-21 09:00:57 -0600

    locale.c: Compile utf8ness on platforms that need it
    These two functions need to be compiled on certain other platforms than
    what was happening.

--- locale.c
+++ locale.c
@@ -4980,6 +4980,8 @@ Perl_setlocale(const int category, const char * locale)
 
 }
 
+#if defined(USE_LOCALE) || defined(HAS_SOME_LANGINFO) || defined(HAS_LOCALECONV)
+
 STATIC utf8ness_t
 S_get_locale_string_utf8ness_i(pTHX_ const char * string,
                                      const locale_utf8ness_t known_utf8,
@@ -5095,8 +5097,8 @@ S_get_locale_string_utf8ness_i(pTHX_ const char * string,
 
     return UTF8NESS_YES;
 
+#    endif
 #  endif
-#endif
 
 }
 
@@ -5148,6 +5150,7 @@ S_is_locale_utf8(pTHX_ const char * locale)
 
 }
 
+#endif
 #ifdef USE_LOCALE
 
 STATIC const char *
